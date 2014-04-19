0a28166d129d53649c1d776dbde068a33ca4b40a
Ray Walters <ray.walters@gmail.com>
2014-04-19 02:11:13 +0000

    Database setup and validations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ActiveRecord::Base
+  validates :title, length: {in: 1..250, too_long: "Only 250 characters allowed."}
+  validates :author, length: {in: 1..75, too_long: "Only 75 characters allowed."}
+  validates :body, length: {in: 1..5000, too_long: "This is 5k, only 5000 characters allowed."}
 end
